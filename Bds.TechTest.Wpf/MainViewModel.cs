398cc14cc216c92a2d97548d184db773436958a5
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2019-08-16 22:28:56 +0100

    Add new wpf app
    # Conflicts:
    #	Bds.TechTest.sln
    #	Bds.TechTest/Controllers/HomeController.cs
    #	Bds.TechTest/Startup.cs

---
+++ Bds.TechTest.Wpf/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Bds.TechTest.Wpf
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Search = new RoutedCommand();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Phrase { get; set; }
+
+        public ICommand Search { get; set; }
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
